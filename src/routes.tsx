5ecb4339ad3ec178eaf92e519612316e14dd9611
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-09-16 17:23:03 -0300

    feat: add navigation in routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import Login from './pages/Login'
+
+const Stack = createStackNavigator()
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const Routes = () => {
-  return <Login/>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default Routes
