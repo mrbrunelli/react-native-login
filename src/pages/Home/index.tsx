cd2c253a8b3833ff99f87a59b401d840a25fb22a
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-09-16 18:10:11 -0300

    feat: add dinamic hello message

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import { useNavigation, useRoute } from '@react-navigation/native'
+import { Text, View, StyleSheet } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 
 interface Params {
   email: string
@@ -10,7 +10,65 @@ interface Params {
 const Home = () => {
   const route = useRoute()
   const { email } = route.params as Params
-  return <Text>Home Page {email}</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.helloView}>
+        <Text style={styles.helloText}>Hello {email} :D</Text>
+      </View>
+      <View style={styles.boxView}>
+        <View style={styles.box}>
+          <View style={styles.boxItem}>
+            <Text style={styles.boxText}>R$ 140.244,59</Text>
+          </View>
+        </View>
+        <View style={styles.box}>
+          <View style={styles.boxItem}>
+            <Text style={styles.boxText}>R$ 140.244,59</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  boxView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    marginTop: 10
+  },
+  box: {
+    width: 150,
+    height: 100,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 20,
+    margin: 10
+  },
+  boxItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  boxText: {
+    fontSize: 16
+  },
+  helloText: {
+    fontSize: 20,
+    textAlign: 'left'
+  },
+  helloView: {
+    width: 400,
+    marginTop: 10,
+    marginLeft: 15
+  }
+})
 
 export default Home
